9fb0eca5236af3127cdc5e4ad96bfc891cf62cc3
ADIL MUHAMMED Y <adilmuhammedy@gmail.com>
2023-04-12 15:20:43 +0530

    login complete

---
+++ app.js
@@ -0,0 +1,91 @@
+
+console.log("hai user");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDjBgmzmlwmso2pJ6IUSaROuAoRKLmynpw",
+    authDomain: "metro-c2e76.firebaseapp.com",
+    databaseURL: "https://metro-c2e76-default-rtdb.firebaseio.com",
+    projectId: "metro-c2e76",
+    storageBucket: "metro-c2e76.appspot.com",
+    messagingSenderId: "443513878126",
+    appId: "1:443513878126:web:af44ba5a20e97a0654587f",
+    measurementId: "G-PF5GKDP950"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  document.getElementById('login').addEventListener('click',(e)=>{
+    e.preventDefault()
+    var email=document.getElementById('email').value;
+    var password=document.getElementById('password').value;
+    console.log(email);
+    console.log(password);
+
+    // firebase.auth().createUserWithEmailAndPassword(email,password)
+    // .then((userCredentials)=>{
+    //   location.reload
+    //   var user=userCredentials.user;
+    //   console.log(user.email);
+    // })
+    // .catch((error)=>{
+    //   var errorMessage=error.message;
+    //   alert(errorMessage);
+
+    // })
+
+
+
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .then((userCredentials)=>{
+      var user=userCredentials.user;
+      alert(user.email);
+      console.log(user.email);
+      alert("login successfull")
+    })
+    .catch((error)=>{
+      var errorMessage=error.message;
+      alert(errorMessage);
+
+    })
+  
+
+  firebase.auth().onAuthStateChanged(function(user){
+    if(user){
+      var email=user.email;
+      var users=document.getElementById('user');
+      var text=document.createTextNode(email);
+      //users.appendChild(text);
+      console.log(user);
+    }else{
+
+    }
+  })
+})
+
+  //   //Google auth
+  // document.getElementById('google-sign').addEventListener('click',(e)=>{
+  //   e.preventDefault()
+    
+  //   const auth = firebase.auth();
+  //   const provider = new firebase.auth.GoogleAuthProvider();
+  //   signInWithPopup(auth, provider)
+  //   .then((result) => {
+  //   // This gives you a Google Access Token. You can use it to access the Google API.
+  //   const credential = GoogleAuthProvider.credentialFromResult(result);
+  //   const token = credential.accessToken;
+  //   // The signed-in user info.
+  //   const user = result.user;
+  //   // IdP data available using getAdditionalUserInfo(result)
+  //   // ...
+  // }).catch((error) => {
+  //   // Handle Errors here.
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  //   // The email of the user's account used.
+  //   const email = error.customData.email;
+  //   // The AuthCredential type that was used.
+  //   const credential = GoogleAuthProvider.credentialFromError(error);
+  //   // ...
+  // });
+    
+  // })
