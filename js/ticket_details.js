33814dd0bea759cba13208080c19e94a5284531e
Devi Nandana <devinandanabk@gmail.com>
2023-05-30 16:32:03 +0530

    added js to ticket confirmation

---
+++ js/ticket_details.js
@@ -0,0 +1,7 @@
+document.getElementById("fare").addEventListener("click", function(){
+    document.querySelector(".popup").style.display="flex";
+  })
+
+document.getElementById("go-back").addEventListener("click", function(){
+    document.querySelector(".popup").style.display="none";
+})
