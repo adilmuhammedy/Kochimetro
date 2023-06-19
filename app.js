dadd014e4dd434b3456012542b2383b1e0958e98
ADIL MUHAMMED Y <adilmuhammedy@gmail.com>
2023-06-19 20:59:11 +0530

    payment gateway done

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const path = require('path');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const app = express();
+const Razorpay=require('razorpay');
+const instance = new Razorpay({
+  key_id: 'rzp_test_KoHAhlJPYY3SRe',
+  key_secret: 'D27EAT8wqDj3zSZfWH8iaUIG',
+});
+
 
 app.use(express.json());
 app.use(express.static(path.join('Projects\Kochi Metro\code', 'code')));
@@ -17,6 +23,8 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const url = "mongodb://localhost:27017";
 const options = {
@@ -24,6 +32,10 @@ const options = {
   useUnifiedTopology: true
 };
 
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  // Handle the error appropriately
+});
 
 
 function generateTicketId() {
@@ -43,7 +55,11 @@ app.post('/submit-form',cors(), async (req, res) => {
   // Perform any necessary backend processing or calculations
   // ...
   let fare=0;
-  
+  console.log("from: " + from);
+  console.log("to: " + to);
+  console.log("timeSlot: " + timeSlot);
+  console.log("ticketType: " + ticketType);
+  console.log("passengers: " + passengers);
   try {
     // Connect to the MongoDB server
     const client = await MongoClient.connect(url, options);
@@ -61,18 +77,33 @@ app.post('/submit-form',cors(), async (req, res) => {
     // Retrieve the fare value from the document
     if (foundDocument) {
       fare = foundDocument.fare;
-      console.log('Fare:', fare);
+      console.log('Fare from database:', fare);
+      if(passengers > 1) {
+        fare = fare * passengers;
+      }
+      if(ticketType == "Two-way") {
+        fare = fare * 2;
+      }
+      if(timeSlot == "6:00-8:00") {
+        fare = fare * 0.5;
+      }
+      if(timeSlot == "21:00-23:00") {
+        fare = fare * 0.5;
+      }
     }
   } catch (error) { 
     console.error(error);
   }
-
-  const redirectUrl = `confirmation.html?fare=${fare}`;
+  console.log("fare reqwqfafq:",fare)
+  const order =  await instance.orders.create({ amount: fare*100, currency: 'INR', receipt: 'receipt1', payment_capture: '0' });
+  console.log("order",order);
+  const redirectUrl = `confirmation.html?fare=${fare}&order_id=${order.id}`;
   const response = {
     ticketId: generateTicketId(),
     confirmationMessage: 'Your ticket has been booked successfully!',
     fare: fare,
-    redirectUrl: redirectUrl
+    redirectUrl: redirectUrl,
+    orderId: order.id,
   };
 
 
@@ -96,6 +127,50 @@ async function generateQRCode(text, imagePath) {
     console.error('Error generating QR code:', error);
   }
 }
+
+app.post('/create/orderId', (req, res) => {
+  console.log("create orderId request",req.body);
+  var options={
+    amount:req.body.amount,
+    currency:"INR",
+    receipt:"rcp1",
+  };
+  instance.orders.create(options).then((order) => {
+    console.log(order);
+    res.send({ orderId: order.id, amount: order.amount });
+  })
+  .catch((error) => {
+    console.log(error);
+    res.status(500).send("Some error occured");
+  });
+});
+
+
+app.post('/api/verify/payment/', (req, res) => {
+  try{
+    res.redirect("http://127.0.0.1:5500/ticket.html?+response.signatureIsValid");
+  //console.log("verify payment request",req.body);
+  //let body=req.body.response.razorpay_order_id+"|"+req.body.response.razorpay_payment_id;
+  //var crypto = require("crypto");
+  //var expectedSignature=crypto.createHmac('sha256', 'D27EAT8wqDj3zSZfWH8iaUIG')
+  //.update(body.toString())
+  //.digest('hex');
+  //console.log("sig received",req.body.response.razorpay_signature);
+  //console.log("sig generated",expectedSignature);
+  //var response={"signatureIsValid":"false"};
+  //if(expectedSignature===req.body.response.razorpay_signature)
+  //response={"signatureIsValid":"true"};
+  }
+  
+  catch(error){
+    console.log(error);
+    res.status(500).send("Error verifying payment");
+  }
+});
+
+app.get('' , (req,res) => {
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
 
 app.listen(8080, () => {
   console.log('Server started on port 8080');
